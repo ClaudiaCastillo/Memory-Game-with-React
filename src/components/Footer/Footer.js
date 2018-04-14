0081417a897ba5050d5cc7b6b202e2c4ad9566fa
castillo.claudia.m.s@gmail.com <castillo.claudia.m.s@gmail.com>
2018-04-13 21:16:20 -0400

    clicky game

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <span>Clicky Game! <img alt="react" src="assets/images/react.svg"></span>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+	<h1 className="header">{props.children}</h1>
+);
+
+export default Header;
